96b8d80f8a028958df00726d58e9cb54039a39bb
Eran Kampf <eran@ekampf.com>
2017-01-05 10:56:22 -0800

    Restructure code

--- graphene_custom_directives/__init__.py
+++ graphene_custom_directives/__init__.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
-from graphql import DirectiveLocation, GraphQLDirective
-from promise import Promise
+from directives import string, list, numbers
+from .middleware import CustomDirectivesMiddleware, CustomDirectiveMeta
 
 __author__ = 'Eran Kampf'
 __email__ = 'eran@ekampf.com'
 __version__ = '0.1.0'
 
-from .string import *
-from .number import *
-from .list import *
-from .middleware import CustomDirectivesMiddleware, CustomDirectiveMeta
 
 __ALL__ = [
     CustomDirectivesMiddleware,
