dc59c58d11a0bec2bc98da8804a3e39ec451fce2
Eran Kampf <eran@ekampf.com>
2017-01-04 16:57:35 -0800

    Number formatting

--- graphene_custom_directives/__init__.py
+++ graphene_custom_directives/__init__.py
@@ -89,6 +89,25 @@ class DefaultDirective(BaseCustomDirective):
         return value
 
 
+class NumberDirective(BaseCustomDirective):
+    """
+    Number formatting
+    """
+    @staticmethod
+    def get_args():
+        return {
+            'as': GraphQLArgument(
+                type=GraphQLNonNull(GraphQLString),
+                description='Value to default to',
+            ),
+        }
+
+    @staticmethod
+    def process(value, directive, root, args, context, info):
+        as_argument = [arg for arg in directive.arguments if arg.name.value == 'as'][0]
+        return format(float(value), as_argument.value.value)
+
+
 class LowercaseDirective(BaseCustomDirective):
     """
     Lowercases result.
--- tests/test_custom_directives_middleware.py
+++ tests/test_custom_directives_middleware.py
@@ -20,14 +20,14 @@ class PersonType(ObjectType):
     name = String()
     age = Int()
     none = String()
+    number = String()
 
 
 class QueryRoot(ObjectType):
     person = Field(PersonType)
 
     def resolve_person(self, *_):
-        return PersonType(name="Eran", age=15)
-
+        return PersonType(name="Eran", age=15, number="1314.15")
 
 
 class TestGrapheneCustomDirectives(unittest.TestCase):
@@ -43,6 +43,7 @@ class TestGrapheneCustomDirectives(unittest.TestCase):
                     name @lowercase
                     age
                     none @default(to: "YEAH")
+                    number @number(as: "0,.1f")
                 }
             }
         ''', middleware=[CustomDirectivesMiddleware()])
@@ -56,6 +57,7 @@ class TestGrapheneCustomDirectives(unittest.TestCase):
         self.assertEqual(person['name'], 'eran')
         self.assertEqual(person['age'], 15)
         self.assertEqual(person['none'], 'YEAH')
+        self.assertEqual(person['number'], '1,314.2')
 
 
     def tearDown(self):
